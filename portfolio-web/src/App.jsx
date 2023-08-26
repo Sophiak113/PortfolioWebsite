bb117de3485183871185484d4501dc72a727e31e
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-08-26 21:21:28 +0100

    Created Basic website outline and outline of ContactPage

--- portfolio-web/src/App.jsx
+++ portfolio-web/src/App.jsx
@@ -1,10 +1,12 @@
 import Background from './components/Background';
 import './App.css';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
     <div className="App">
       <Background/>
+      <ContactPage></ContactPage>
     </div>
   );
 }
