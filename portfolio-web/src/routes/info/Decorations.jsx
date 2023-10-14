644cb01530a9d5b6157b4274ccac5456db150cdc
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-10-14 22:45:54 +0100

    Began working on Hamburger Menu and beginning media queries

--- portfolio-web/src/routes/info/Decorations.jsx
+++ portfolio-web/src/routes/info/Decorations.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import FixedImagesBot from '../../components/FixedImagesBot';
 import FixedImagesTop from '../../components/FixedImagesTop';
+import NavBar from '../../components/NavBar';
 import { Outlet } from 'react-router-dom';
 
 
 const Decorations = () => {
   return (
               <div className="decorative-container">
+                <NavBar></NavBar>
+                {/* Temporary navbar till expanded is created */}
 
                   <FixedImagesTop/>
                     <div className="outlet-container">
-                    {/* <ExpandingNav /> */}
                         <Outlet></Outlet>
                         
                     </div>
