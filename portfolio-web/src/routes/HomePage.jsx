0e9c655ac2ed80dcf4976a53d4727a713c0c6bae
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-10-19 00:43:23 +0100

    Put same media query for the majority (as still need to finish), added a responsive title in homePage.jsx using javascript

--- portfolio-web/src/routes/HomePage.jsx
+++ portfolio-web/src/routes/HomePage.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import NavBar from '../components/NavBar'
 import GlitchText from '../components/GlitchText'
 
 const HomePage = () => {
+  //screenWidth and sameDiv are a part of responsiveness, when the screen becomes wide enough Sophia Kinsey comes onto 1 line instead of 2
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [sameDiv, setSameDiv] = useState(screenWidth > 1024)
+
+    //Checking for resize and if the screen width is over 1024, Sophia Kinsey is 1 div not 2
+  useEffect(()=>{
+    function handleResize(){
+      const newWidth = window.innerWidth;
+      setScreenWidth(newWidth);
+      setSameDiv(newWidth > 1024);
+    };
+
+    // Add an event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+
+  },[]);
+  
   return (
     <div className="home-page-container">
       <div className="title-wrapper">
         {/* <img src="/Title.png" alt="Sophia Kinsey - Software Engineer" /> */}
-        <div className="title-name">
-          <GlitchText tag='h3' text='Sophia' id='Sophia'/>
-          <GlitchText tag='h3' text='Kinsey' id='Kinsey'/>
-        </div>
+        
+        {sameDiv ? (
+            <GlitchText tag='h3' text='Sophia Kinsey' id='' />
+          ) : (
+            <div className="title-name">
+              <GlitchText tag='h3' text='Sophia' id='Sophia' />
+              <GlitchText tag='h3' text='Kinsey' id='Kinsey' />
+            </div>
+          )}
+          
 
         <GlitchText tag='h2' text='Web Developer' id='WebDeveloper'/>
 
