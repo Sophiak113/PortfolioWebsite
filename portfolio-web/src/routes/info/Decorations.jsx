9aab6d2fe25008f6dca5916c31ca2f38955f5198
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-11-07 06:40:06 +0000

    Finished css for existing parts of the drop down/ pop up menu, need to add in a cross to close menu

--- portfolio-web/src/routes/info/Decorations.jsx
+++ portfolio-web/src/routes/info/Decorations.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import FixedImagesBot from '../../components/FixedImagesBot';
 import FixedImagesTop from '../../components/FixedImagesTop';
 import NavBar from '../../components/NavBar';
 import { Outlet } from 'react-router-dom';
+import HamburgerMenu from '../../components/HamburgerMenu';
 
 
 const Decorations = () => {
   return (
               <div className="decorative-container">
-                <NavBar></NavBar>
+                {/* <NavBar></NavBar> */}
                 {/* Temporary navbar till expanded is created */}
 
                   <FixedImagesTop/>
+                  <ExpandingNav />
+
+                  {/* <HamburgerMenu></HamburgerMenu> */}
                     <div className="outlet-container">
+                      
+
                         <Outlet></Outlet>
                         
                     </div>
@@ -24,13 +30,46 @@ const Decorations = () => {
 }
 
 const ExpandingNav = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick =()=>{
+    setIsClicked(true)
+
+  }
+
+
+
+  // useEffect(()=>{
+  //   function handleClick(){
+      
+  //   }
+
+  // },[]);
   return (
-    <div className='expanding-nav'>
+    <div className='expanding-nav-container'>
+      <div className="expanding-nav-tab" onClick={()=>handleClick()}>
+      {/* Nav */}
+      <div className="hamburger-line"></div>
+      <div className="hamburger-line"></div>
+      <div className="hamburger-line"></div>
+      </div>
+    {isClicked ? <DropDownNav/>: ''
+    }
       
     </div>
   )
 }
 
+const DropDownNav=()=>{
+  return(
+    <div className="drop-down-container-nav">
+      
+      <NavBar/>
 
+    </div>
+
+  );
+
+}
 
 export default Decorations
